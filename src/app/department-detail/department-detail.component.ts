934949d8cd94ec5aedd8575a847c67bee4daa686
Bastian Bräunel <bastian.braeunel@gmx.de>
2019-06-14 12:42:34 +0200

    Modified the department-detail.component.ts to look for the observable of the paramMap instead of using the snapshot of the url.
    	geändert:       src/app/department-detail/department-detail.component.ts

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `
     <h3>Your selected the department with ID: {{ departmentid }}</h3>
+    <a class='burron' (click)='goPrevious()'> \< Previous</a>
+    <a class='burron' (click)='goNext()'> Next \></a>
   `,
   styles: []
 })
@@ -18,23 +20,50 @@ export class DepartmentDetailComponent implements OnInit {
    *
    * @param activeRoute the injected activeRouter
    */
-  constructor(private activeRoute: ActivatedRoute) { }
+  constructor(private activeRoute: ActivatedRoute, private router: Router) { }
 
   /**
    * When the view is loeaded, this method is fired
    * and we use it to analyze the url parameter
    */
   ngOnInit() {
-    /**
+    /** -- snapshot --
      * parseInt -> transforms the string to an int
      * activeRout -> the ActiveRout reference
      * snapshot -> take a snapshot of the current url
      * paramMap -> get a map of url marameters that are contained by the snapshot
      * get -> get the value of a specific parameter from the paramMap
      */
-    let id = parseInt(this.activeRoute.snapshot.paramMap.get('id'));
+    //let id = parseInt(this.activeRoute.snapshot.paramMap.get('id'));
 
-    this.departmentid = id;
+    /** -- observable --
+     * activeRoute -> the reference to the ActiveRoute
+     * paramMap -> the map of the current parameters of the url
+     * subscribe -> subscribe to an observabel
+     * (params: ParamMap) -> subcribe to the observable paramMap
+     * => -> lambda function that wil be called if the observable has changed
+     * params.get('id') -> get the new id from the observable
+     */
+    this.activeRoute.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.departmentid = id;
+    });
+  }
+
+  /**
+   * use the router to decrease the id parameter
+   */
+  goPrevious(){
+    let previousID = this.departmentid -1;
+    this.router.navigate(['/departments', previousID])
+  }
+
+  /**
+   * use the router to increase the id parameter
+   */
+  goNext() {
+    let nextID = this.departmentid + 1;
+    this.router.navigate(['/departments', nextID])
   }
 
 }
